8f2d9821f03fb8adac70432a4065fd5d2a9af078
Jisoo <jisoooh0202@gmail.com>
2022-01-12 10:36:32 +0700

    access todos inside html files

--- todo/views.py
+++ todo/views.py
@@ -5,5 +5,8 @@ from .models import Todo
 
 def todo_list(request):
 	todos = Todo.objects.all()
+	context = {
+		"todo_list": todos
+	}
 	print(todos)
-	return render(request, "todo_list.html")
+	return render(request, "todo_list.html", context)
