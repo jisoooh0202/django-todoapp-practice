aeb167f4680a8903206a5a727a213e37dd74c3d5
Jisoo <jisoooh0202@gmail.com>
2022-01-12 17:24:49 +0700

    update for delete todo

--- todo/urls.py
+++ todo/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from .views import todo_detail, todo_list, todo_create, todo_update
+from .views import todo_detail, todo_list, todo_create, todo_update, todo_delete
 
 app_name = 'todos'
 
@@ -8,5 +8,6 @@ urlpatterns = [
 	path('', todo_list),
 	path('create/', todo_create),
 	path('<id>/', todo_detail),
-	path('<id>/update/', todo_update)
+	path('<id>/update/', todo_update),
+	path('<id>/delete/', todo_delete),
 ]
